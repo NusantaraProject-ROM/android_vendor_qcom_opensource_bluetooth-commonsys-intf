58460d0f2419293ee3c53e6b263c744c137fbac6
Kiran Kelageri <kirankelageri@codeaurora.org>
2019-06-19 17:24:46 -0700

    Blueooth-Wipower: Remove Wipower feature
    Remove Wipower feature since dpor'd
    
    Change-Id: I861564df77158df0a02fe5d78649231f1e774ef4

--- bt-commonsys-intf-legacy-board.mk
+++ bt-commonsys-intf-legacy-board.mk
@@ -6,9 +6,6 @@ TARGET_USE_QTI_BT_STACK := true
 #FM
 BOARD_HAVE_QCOM_FM := true
 
-#WIPOWER
-BOARD_USES_WIPOWER := false
-
 ifeq ($(filter sdm660 msm8998, $(TARGET_BOARD_PLATFORM)),)
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 BOARD_HAS_QCA_FM_SOC := "cherokee"
