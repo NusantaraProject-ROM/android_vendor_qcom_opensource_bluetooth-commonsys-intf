46e547d81615806bbd6e2ec1e94be7ed9f156e78
Mahesh Kumar Sharma <smahesh@codeaurora.org>
2019-08-12 22:04:23 -0700

    ANT: Fix ANT issue
    Fix ANT issue by correcting compilation condition for
    legacy target.
    
    Change-Id: Id4256d0b1dfa25d5128840bd5cb1fb54afd25ea7

--- bt-commonsys-intf-legacy-board.mk
+++ bt-commonsys-intf-legacy-board.mk
@@ -6,16 +6,16 @@ TARGET_USE_QTI_BT_STACK := true
 #FM
 BOARD_HAVE_QCOM_FM := true
 
-ifeq ($(filter sdm660 msm8998, $(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter sdm660 msm8998, $(TARGET_BOARD_PLATFORM)),)
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 endif
 
-ifeq ($(filter msm8996, $(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8996, $(TARGET_BOARD_PLATFORM)),)
 #BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 endif
 
-ifeq ($(filter msm8937 msm8953 msm8909, $(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8937 msm8953 msm8909, $(TARGET_BOARD_PLATFORM)),)
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 endif
 
