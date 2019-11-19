cce5326a36e415beefe75c01e467450c3561ed1c
Nitin Shivpure <nshivpur@codeaurora.org>
2019-11-19 11:56:59 +0530

    btcommon: Move qti specific modules under qti flag
    - Move qti specific modules under qti flag
    - Add conf into product_soong_namespace
    - Guard BA & BT test app with low_ram flag
    
    CRs-fixed: 2606311
    Change-Id: I8dc40299e328919de6ecd257e5f435a63b61a767

--- bt-system-opensource-product.mk
+++ bt-system-opensource-product.mk
@@ -17,21 +17,10 @@ PRODUCT_PACKAGES += Bluetooth
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
 TARGET_USE_QTI_BT_STACK := true
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/qva
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/qva/config
-
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+# BT Related Libs
 PRODUCT_PACKAGES += libbluetooth_qti
-
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
 PRODUCT_PACKAGES += libbluetooth_qti_jni
-endif #TARGET_USE_QTI_BT_STACK
-
-ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
-else
-PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
-endif #TARGET_USE_QTI_BT_STACK
-
 PRODUCT_PACKAGES += bt_logger
 PRODUCT_PACKAGES += libbt-logClient
 PRODUCT_PACKAGES += libbtconfigstore
@@ -39,13 +28,30 @@ PRODUCT_PACKAGES += vendor.qti.hardware.btconfigstore@1.0
 PRODUCT_PACKAGES += com.qualcomm.qti.bluetooth_audio@1.0
 PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_audio@2.0
 PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
+
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/system/bt/conf
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/qva
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/qva/config
+
 # BT Related Test app & Tools
-PRODUCT_PACKAGES_DEBUG += BATestApp
-PRODUCT_PACKAGES_DEBUG += BTTestApp
 PRODUCT_PACKAGES_DEBUG += btsnoop
 PRODUCT_PACKAGES_DEBUG += gatt_tool_qti_internal
 PRODUCT_PACKAGES_DEBUG += l2test_ertm
 PRODUCT_PACKAGES_DEBUG += rfc
+
+ifneq ($(TARGET_HAS_LOW_RAM), true)
+PRODUCT_PACKAGES_DEBUG += BTTestApp
+PRODUCT_PACKAGES_DEBUG += BATestApp
+endif #TARGET_HAS_LOW_RAM
+
+else
+PRODUCT_SOONG_NAMESPACES += packages/apps/Bluetooth
+PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/generic
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/generic/config
+endif #TARGET_USE_QTI_BT_STACK
+
 else
 PRODUCT_PACKAGE_OVERLAYS += vendor/qcom/opensource/commonsys-intf/bluetooth/overlay/generic
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := vendor/qcom/opensource/commonsys-intf/bluetooth/build/generic/config
